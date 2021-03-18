e7b1d593e435c80dda2dd3dc7d639657c9141086
Mister1SI <wazzup2690@gmail.com>
2021-03-18 15:32:01 +0000

    0.0.2: Create month 1

--- main.cs
+++ main.cs
@@ -3,11 +3,15 @@ using System;
 class MainClass {
   public static void Main (string[] args) {
     string name, companyName;
-    double companyValue, personalProfit;
+    float workerCost = 100000;
+    double companyValue = 750000, personalProfit = 45000, envCosts = 20000;
+    int month = 1;
     Console.WriteLine("Welcome to the business simulator.\n==========\nPlease enter your name:");
     name = Console.ReadLine();
     Console.WriteLine("Please enter your company name:");
     companyName = Console.ReadLine();
-    Console.WriteLine("Thank you.");
+    Console.WriteLine("Thank you. Press enter to begin.");
+    Console.ReadKey();
+    Console.WriteLine("==========\n\nStart of month 1\nCost of workers: $" + workerCost + "\nEnvironment costs: $" + envCosts + "\nCompany Value: $" + companyValue + "\nYour profit per month: $" + personalProfit);
   }
 }
